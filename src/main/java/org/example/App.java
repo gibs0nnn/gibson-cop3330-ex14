047b4dc00d02f9a4d9208fdcb2a8aa83330b7fa5
gibs0nnn <gibs0n@knights.ucf.edu>
2021-09-11 00:32:53 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,39 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 14 Solution
+ *  Copyright 2021 Nathaniel Gibson
+ */
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        //Input
+        System.out.print("What is the order amount? ");
+        double ordAmt = scanner.nextDouble();
+        //ordAmt = Math.round(ordAmt * 100) / 100;
+        System.out.print("What is the state? ");
+        String state = scanner.next();
+
+        //Subtotal Display
+        System.out.println("The subtotal is $" + df.format(ordAmt) + ".");
+
+        //Calculations
+        double tax = ordAmt * .055;
+        double total = Math.round((ordAmt + tax) * 100.0) / 100.0;
+
+        //Condition for WI
+        if(state.contains("WI")) {
+            System.out.println("The tax is $" + tax + "." + "\nThe total is $" + total + ".");
+        }
+    }
+}
